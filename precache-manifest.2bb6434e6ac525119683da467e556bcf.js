00c9549f4762fb4cdfa9afe6a2037f11db55838a
HK-MBURU <96179053+HK-MBURU@users.noreply.github.com>
2023-06-06 15:47:37 +0300

    Updates

---
+++ precache-manifest.2bb6434e6ac525119683da467e556bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96371d73729b9eeafaf1",
+    "url": "/static/css/main.f97eac25.chunk.css"
+  },
+  {
+    "revision": "96371d73729b9eeafaf1",
+    "url": "/static/js/main.96371d73.chunk.js"
+  },
+  {
+    "revision": "d4e9ccc855535b7ae604",
+    "url": "/static/js/1.d4e9ccc8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3888cbbaa070f1fd8689b5d3ede88e90",
+    "url": "/static/media/marvel.3888cbba.jpg"
+  },
+  {
+    "revision": "d62e00b6d9b04448c4f69147baee9ad7",
+    "url": "/index.html"
+  }
+];
